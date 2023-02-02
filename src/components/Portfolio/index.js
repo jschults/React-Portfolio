37c080250b703b0ecfe9a8ac63c9781f2aaf7247
Julian Schultheis <jschults00@gmail.com>
2023-02-03 01:00:51 +0530

    Final Deployment

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Project from '../Project';
 
 function Portfolio() {
-	const projects = [];
 
 	return (
 		<section>
@@ -10,30 +9,7 @@ function Portfolio() {
 				<h1 className="page-header">My Portfolio</h1>
 			</div>
 			<div>
-				<ul className="flex-row mobile-row">
-					<li className="padding">
-						<Project projects={projects[0]}></Project>
-					</li>
-					<li className="padding">
-						<Project projects={projects[1]}></Project>
-					</li>
-				</ul>
-				<ul className="flex-row mobile-row">
-					<li className="padding">
-						<Project projects={projects[2]}></Project>
-					</li>
-					<li className="padding">
-						<Project projects={projects[3]}></Project>
-					</li>
-				</ul>
-				<ul className="flex-row mobile-row">
-					<li className="padding">
-						<Project projects={projects[4]}></Project>
-					</li>
-					<li className="padding">
-						<Project projects={projects[5]}></Project>
-					</li>
-				</ul>
+				
 			</div>
 		</section>
 	);
